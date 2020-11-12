db26c415081f89c8eb73f228e68c7771d8421b61
Richard Guerre <guerre.richard@yahoo.com>
2020-11-12 02:44:29 +0100

    initial setup 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Shortcut from './Shortcut';
 
 const root = document.createElement('div');
 root.id = 'one-figma-shortcut-a-day-root';
 document.body.appendChild(root);
 
 ReactDOM.render(
-  <App />,
+  <Shortcut />,
   document.getElementById('one-figma-shortcut-a-day-root')
 );
